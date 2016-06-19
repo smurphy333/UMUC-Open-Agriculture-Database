5710383578d2bc3ba41284d373b35d61305a455a
smurphy333 <smurphy333@githum.com>
2016-06-18 23:32:50 -0400

    Project-Branch-Server

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,4 +1,4 @@
-opyright (c) 2016, Open Agriculture Database team Food UMUC 495
+#Copyright (c) 2016, Open Agriculture Database team Food UMUC 495
 # OpenAg is an Open source Social network written in Ruby on Rails This file is licensed
 # under GNU GPL v2 or later. See the LICENSE.
 
